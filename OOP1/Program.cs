c7dc7b50d3a5fb52a9f98b1db4baf49edb2e3c40
Pelda Kabak <peldakabak@hotmail.com>
2021-01-30 16:09:51 +0300

    Add project files.

---
+++ OOP1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 3;
+            product1.ProductName = "Skirt";
+            product1.UnitPrice = 55;
+            product1.UnitsInStock = 15;
+
+            Product product2 = new Product();
+            product2.Id = 2;
+            product2.CategoryId = 5;
+            product2.ProductName = "jean";
+            product2.UnitPrice = 79.99;
+            product2.UnitsInStock = 40;
+
+            ProductManager productManager = new ProductManager();
+
+            productManager.Add(product2);
+
+            Console.WriteLine("---------------------");
+
+            productManager.Update(product1);
+
+
+
+
+        }
+    }
+}
